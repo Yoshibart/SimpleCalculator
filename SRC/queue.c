4f5aed04036c7f5212cd58ba34bbf874781a329b
Barnet Obeka <oworibarnet@gmail.com>
2021-08-19 21:23:01 +0100

    implementing enqueue and dequeue for the queue

--- SRC/queue.c
+++ SRC/queue.c
@@ -14,4 +14,31 @@
 //
 //#endif
 #include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
 
+struct queue * queue_buffer = NULL;
+struct queue * queue_rear = NULL;
+
+
+void enqueue(float element){
+	struct queue * ptr = (struct queue) malloc(sizeof(struct queue));
+	ptr->element = element;
+	ptr->next = NULL;
+	(queue_rear) ? queue_rear->next = ptr : queue_buffer = ptr;
+	queue_rear = ptr
+}
+
+float dequeue(void){
+	if(queue_buffer){	
+		struct queue  * ptr = queue_buffer;
+		float element = ptr->element;
+		queue_buffer = ptr->next;
+		if(ptr == queue_rear){
+			queue_rear = NULL;	
+		}
+		free(ptr);
+		return element;
+	}
+	return 0;
+}
